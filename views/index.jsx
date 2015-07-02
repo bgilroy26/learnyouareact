bdd4b9ae07545cdfbf263aefacb8dc0e2b38c48c
brendan gilroy <bdgilroy@gmail.com>
2015-07-02 16:56:58 -0400

    finished props challenge

--- views/index.jsx
+++ views/index.jsx
@@ -1,14 +1,21 @@
 var React = require('react');
-
 var TodoBox = React.createClass({
-    // Omitted
-);
+    render: function() {
+        return (
+                <div className="todoBox">
+                    <h1>Todos</h1>
+                    <TodoList />
+                    <TodoForm />
+                </div>
+               );
+    }
+});
 
 var TodoList = React.createClass({
     render: function() {
         return (
                 <div className="todoList">
-                    <table style = {{border: "2px solid black"}}>
+                    <table style = {{ border: "2px solid black" }}>
                         <tbody>
                             <Todo title="Shopping">Milk</Todo>
                             <Todo title="Hair cut">13:00</Todo>
@@ -23,15 +30,22 @@ var Todo = React.createClass({
     render: function() {
         return ( 
             <tr>
-                <td style="border: 1px solid black;">{ this.props.title }</td>
-                <td style="border: 1px solid black;">{ this.props.children }</td>
+                <td style={{ border: "1px solid black" }}>{ this.props.title }</td>
+                <td style={{ border: "1px solid black" }}>{ this.props.children }</td>
             </tr>
         );
     }
 });
 
+
 var TodoForm = React.createClass({
-   // Omitted 
+    render: function() {
+        return (
+                <div className= "todoForm">
+                    I am a TodoForm.
+                </div>
+               );
+    }
 });
 
 module.exports = TodoBox;
